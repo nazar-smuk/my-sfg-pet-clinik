03cef5b3c256e2206df182a33272f15e07617747
nazar <nazarsmuk94@gmail.com>
2022-03-15 19:11:34 +0200

    Init Pet Clinic

---
+++ src/main/java/com/example/mysfgpetclinik/MySfgPetClinikApplication.java
@@ -0,0 +1,13 @@
+package com.example.mysfgpetclinik;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MySfgPetClinikApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(MySfgPetClinikApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/example/mysfgpetclinik/MySfgPetClinikApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mysfgpetclinik;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MySfgPetClinikApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
